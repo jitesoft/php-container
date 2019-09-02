9fe001a4d567a0fab6503ee31dd4810806ad57c7
Johannes Tegnér <johannes@jitesoft.com>
2019-09-02 09:21:05 +0200

    Fixed sniffer validations in ContainerEntry.php

--- src/ContainerEntry.php
+++ src/ContainerEntry.php
@@ -6,45 +6,51 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 namespace Jitesoft\Container;
 
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
+use Jitesoft\Exceptions\Psr\Container\ContainerException;
+use Jitesoft\Exceptions\Psr\Container\NotFoundException;
 
 /**
  * Class ContainerEntry
+ *
  * @internal
  */
 class ContainerEntry {
-
     /** @var string */
     protected $abstract;
     /** @var mixed */
     protected $concrete;
-    /** @var bool */
+    /** @var boolean */
     protected $isSingleton;
-    /** @var bool */
+    /** @var boolean */
     protected $isCreated;
 
     /**
      * ContainerEntry constructor.
      *
-     * @param string $abstract
-     * @param $concrete
-     * @param bool $isSingleton
+     * @param string  $abstract    Abstract to bind.
+     * @param mixed   $concrete    Concrete to bind to abstract.
+     * @param boolean $isSingleton If a singleton instance or not.
      *
      * @internal
      */
-    public function __construct(string $abstract, $concrete, bool $isSingleton = false) {
+    public function __construct(string $abstract,
+                                $concrete,
+                                bool $isSingleton = false) {
         $this->abstract    = $abstract;
         $this->concrete    = $concrete;
-        $this->isCreated   = (!is_string($concrete) || !class_exists($concrete));
+        $this->isCreated   = (
+            (!is_string($concrete) || !class_exists($concrete))
+        );
         $this->isSingleton = $isSingleton;
     }
 
     /**
-     * @param Injector $injector
+     * @param Injector $injector Injector to use for resolving.
+     *
+     * @throws ContainerException Thrown in case the container fails in injection.
+     * @throws NotFoundException  Thrown in case the value is not found.
+     *
      * @return mixed|null|object|string
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
      *
      * @internal
      */
